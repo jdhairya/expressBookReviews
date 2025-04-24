89779f2b3412ac86a0ba0679b2cbd0f6d8d63146
jdhairya <jdhairya765@gmail.com>
2025-04-24 12:23:45 +0530

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,32 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+  1: { author: "Frank Herbert", title: "Dune", reviews: {1: "Great Read", 2: "Throughly enjoyed"} },
+  2: { author: "Joseph Heller", title: "Catch-22", reviews: {} },
+  3: { author: "George Orwell", title: "1984", reviews: {} },
+  4: {
+    author: "Terry Brooks",
+    title: "The Talismans of Shannara",
+    reviews: {},
+  },
+  5: {
+    author: "Philip Pullman",
+    title: "The Golden Compass",
+    reviews: {},
+  },
+  6: {
+    author: "Philip Pullman",
+    title: "The Subtle Knife",
+    reviews: {},
+  },
+  7: {
+    author: "Jane Austen",
+    title: "Pride and Prejudice",
+    reviews: {},
+  },
+  8: {
+    author: "Margaret Mitchell",
+    title: "Gone With the Wind",
+    reviews: {},
+  },
+};
 
-module.exports=books;
+module.exports = books;
